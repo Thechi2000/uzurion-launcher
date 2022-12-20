6bf6d4ec787f088107aa690d980bc08f02b74a66
Ludovic Mermod <ludovic.mermod@epfl.ch>
2022-12-20 18:51:27 +0100

    Add main menu skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,10 @@ import Image from "next/image";
 import reactLogo from "../assets/react.svg";
 import tauriLogo from "../assets/tauri.svg";
 import nextLogo from "../assets/next.svg";
+import Settings from "./settings";
+import Accounts from "./accounts";
+import Socials from "./socials";
+import Status from "./status";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -16,62 +20,23 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      <h1>Uzurion</h1>
 
-      <div className="row">
-        <span className="logos">
-          <a href="https://nextjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={nextLogo}
-              className="logo next"
-              alt="Next logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://tauri.app" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={tauriLogo}
-              className="logo tauri"
-              alt="Tauri logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://reactjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={reactLogo}
-              className="logo react"
-              alt="React logo"
-            />
-          </a>
-        </span>
-      </div>
-
-      <p>Click on the Tauri, Next, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
+      <div id="app-menu">
+        <div className="vertical-container">
+          <Accounts/>
+          <Settings/>
+        </div>
+        <div className="vertical-container center">
+          <button>Play</button>
+          <Socials/>
+        </div>
+        <div className="vertical-container">
+          <Status/>
         </div>
       </div>
-
-      <p>{greetMsg}</p>
     </div>
-  );
+    );
 }
 
 export default App;
