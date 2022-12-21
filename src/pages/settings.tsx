bca7821bae7bf7b5912ebd22346085b3630511d8
Ludovic Mermod <ludovic.mermod@epfl.ch>
2022-12-21 10:34:30 +0100

    Add settings logo

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,5 +1,10 @@
+import Image from "next/image"
+import settingsLogo from "../assets/settings.svg"
+
 export default function Settings(){
     return (
-        <p>Settings</p>
+        <div>
+            <Image id="settings-logo" src={settingsLogo} width={60} height={60}/>
+        </div>
     )
 }
