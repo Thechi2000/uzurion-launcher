a12b1c7a66d2a3f5f6cb90f83e43e378afc1cd12
Ludovic Mermod <ludovic.mermod@epfl.ch>
2022-12-22 11:17:09 +0100

    Complete Settings UI

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,12 +1,39 @@
+import { useState } from "react"
+
 export default function Settings({visibility, setVisibility}){
+    const [selectedPane, setSelectedPane] = useState("JVM")
 
     function parameters(){
-        return (
-            <div className="settings-parameters">
-                <div><input placeholder="Java path"/></div>
-                <div><input placeholder="Java path"/></div>
-            </div>  
-        )
+        switch (selectedPane) {
+            case "JVM":
+                return (
+                    <div className="settings-parameters">
+                        <div>
+                            <span className="setting-name">RAM: </span>
+                            <input type="range" min={1} max={16}/>
+                        </div>
+        
+                        <div>
+                            <span className="setting-name">Resolution:</span>
+                            <select id="game-resolution">
+                                <option value="1920x1080">1920x1080</option>
+                                <option value="2560x1440">2560x1440</option>
+                            </select>
+                        </div>
+                    </div>  
+                )
+            case "Launcher":
+                return(
+                    <div className="settings-parameters">
+                        <p>Nothing so far :)</p>
+                    </div>
+                )
+        }
+        
+    }
+
+    function selectedClass(name) {
+        return selectedPane == name ? "selected-settings-pane" : ""
     }
 
     return (
@@ -14,9 +41,11 @@ export default function Settings({visibility, setVisibility}){
             <div id="settings">
                 {parameters()}
                 <div id="settings-navigator">
-                    <p>JVM</p>
-                    <p>Launcher</p>
-                    <p onClick={() => setVisibility(false)}>Quit</p>
+                    <div id="settings-pages-selectors">
+                        <div className={selectedClass("JVM")} onClick={() => setSelectedPane("JVM")}><p>JVM</p></div>
+                        <div className={selectedClass("Launcher")} onClick={() => setSelectedPane("Launcher")}><p>Launcher</p></div>
+                    </div>
+                    <div id="settings-quit"><p onClick={() => setVisibility(false)}>Quit</p></div>
                 </div>
             </div>
         </div>
