f7bb15c9774f878799dbe80a83f31ce9e080f6d0
Ludovic Mermod <ludovic.mermod@epfl.ch>
2022-12-25 10:21:14 +0100

    Add send_event macro

--- src-tauri/src/consts.rs
+++ src-tauri/src/consts.rs
@@ -6,3 +6,12 @@ pub mod events {
     pub const SERVER_STATUS_REFRESH: &str = "server-status";
     pub const SETTINGS_UPDATE: &str = "settings-update";
 }
+
+#[macro_export]
+macro_rules! send_event {
+    ($app: expr, $event: expr, $payload: expr) => {
+        if let Err(e) = ::tauri::Manager::emit_all(&$app, $event, $payload){
+            ::log::error!("Could not send {} event: {:?}", $event, e);
+        }
+    };
+}
