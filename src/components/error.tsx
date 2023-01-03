5a820b2579da07884bb1592fccf4fc2d0604a181
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-01-03 10:52:41 +0100

    Add error window style

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -1,10 +1,12 @@
 export default function Error(props: {name: string; description: string; hide: CallableFunction}){
     return (
         <div className="modal-window-canvas">
-            <div className="modal-window">
-                <h1>{props.name}</h1>
-                <p>{props.description}</p>
-                <button onClick={() => props.hide()}>Ok</button>
+            <div id="error-modal-window" className="modal-window">
+                <div>
+                    <h2>{props.name}</h2>
+                    <p>{props.description}</p>
+                    <button onClick={() => props.hide()}>Ok</button>
+                </div>
             </div>
         </div>
     )
