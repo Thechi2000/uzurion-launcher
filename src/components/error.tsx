e5087b1591032157c90d28e7925e5282df8456eb
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-01-04 11:20:23 +0100

    Improve error modal window

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -4,7 +4,7 @@ export default function Error(props: {name: string; description: string; hide: C
             <div id="error-modal-window" className="modal-window">
                 <div>
                     <h2>{props.name}</h2>
-                    <p>{props.description}</p>
+                    <div><p>{props.description}</p></div>
                     <button onClick={() => props.hide()}>Ok</button>
                 </div>
             </div>
